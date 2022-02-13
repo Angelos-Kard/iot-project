cca7d5ec1d7a66b3a968036a51cc28f147f6f30f
Angelos Kard <angelkard77@gmail.com>
2022-02-13 06:38:06 +0200

    Added 2 new colors Added the colors: - boxGray - red

--- eBikeApp/assets/colors/colors.js
+++ eBikeApp/assets/colors/colors.js
@@ -3,9 +3,11 @@ const colors = {
     "white": "#FFFFFF",
     "gray": "#D6D6D6",
     "darkGray": "#333333",
+    "boxGray": "#2B2B2B",
     "purple": "#5941A9",
     "cyan": "#0F807A",
-    "cyanBorder": "#18F2B2"
+    "cyanBorder": "#18F2B2",
+    "red": "#B42323",
 };
 
   
