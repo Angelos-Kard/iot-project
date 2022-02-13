c097db55d02c01f8ded3bafe71ce45c955cbf993
Angelos Kard <angelkard77@gmail.com>
2022-02-13 03:51:13 +0200

    Created colors.js It contains all the colors that are used

---
+++ eBikeApp/assets/colors/colors.js
@@ -0,0 +1,12 @@
+const colors = {
+    "black": "#101010",
+    "white": "#FFFFFF",
+    "gray": "#D6D6D6",
+    "darkGray": "#333333",
+    "purple": "#5941A9",
+    "cyan": "#0F807A",
+    "cyanBorder": "#18F2B2"
+};
+
+  
+export default colors;
